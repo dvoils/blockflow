262906f978e859c045b4dea4b96141bc407adf5a
Danny Voils <dannyjvoils@gmail.com>
2025-01-13 19:02:18 -0800

    spark is running in kubernetes

--- docker/spark-app/spark_app.py
+++ docker/spark-app/spark_app.py
@@ -1,14 +1,7 @@
 from pyspark.sql import SparkSession
 
-spark = SparkSession.builder \
-    .appName("MyApp") \
-    .config("spark.jars.ivy", "/nonexistent") \
-    .config("spark.jars", "local:///opt/spark/jars/hadoop-aws-3.3.4.jar,local:///opt/spark/jars/aws-java-sdk-bundle-1.11.1026.jar") \
-    .getOrCreate()
-
-
 # Create a Spark session
-# spark = SparkSession.builder.appName("SimpleApp").getOrCreate()
+spark = SparkSession.builder.appName("SimpleApp").getOrCreate()
 
 # Create a DataFrame
 data = [("Hello, PySpark!",)]
